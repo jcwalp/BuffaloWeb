a2d3414c910f2b231c960a95669c655c76691658
jon <43426006+jcwalp@users.noreply.github.com>
2018-11-29 15:23:21 -0500

    Debugging

--- scriptWeather.js
+++ scriptWeather.js
@@ -6,10 +6,9 @@ imgArr[0] = "img1.jpg";
 imgArr[1] = "img2.jpg";
 imgArr[2] = "img4.jpg";
 
-function getRandomImage(imgAr, path) {
-    path = path || 'images/'; // default path here
+function getRandomImage(imgAr) {
     var num = Math.floor( Math.random() * imgAr.length );
     var img = imgAr[ num ];
-    var imgStr = '<img src="' + path + img + '" alt = "">';
+    var imgStr = '<img src="' + img + '" alt = "">';
     document.write(imgStr); document.close();
 }
