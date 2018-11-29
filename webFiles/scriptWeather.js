577f1fe42baf4ba44bc0fb161fba1065ead85891
jon <43426006+jcwalp@users.noreply.github.com>
2018-11-29 13:06:18 -0500

    almost done

---
+++ webFiles/scriptWeather.js
@@ -0,0 +1,16 @@
+
+ // JavaScript to interpolate random images into a page.
+var ic = 4; // Number of alternative images
+var imgArr = new Array(ic); // Array to hold filenames
+imgArr[0] = "img1.jpg";
+imgArr[1] = "img2.jpg";
+imgArr[2] = "img3.jpg";
+imgArr[3] = "img4.jpg"
+
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
